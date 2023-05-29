49d7c3b5d9d5bb31f9f2caa65ca0fd049d08f06d
zeroincombenze <info@shs-av.com>
2023-05-29 11:19:18 +0200

    [FIX] import decimal_precision

--- account_analytic_analysis_recurring/account_analytic_analysis_recurring.py
+++ account_analytic_analysis_recurring/account_analytic_analysis_recurring.py
@@ -25,7 +25,7 @@ import time
 
 from openerp.osv import orm, fields
 from openerp.tools.translate import _
-from openerp.addons.decimal_precision import decimal_precision as dp
+import decimal_precision as dp
 
 _logger = logging.getLogger(__name__)
 
